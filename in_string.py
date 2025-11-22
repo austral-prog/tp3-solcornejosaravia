f510509d13d2866c9e02085875ab77b741b157d3
solcornejosaravia <scornejosaravia@mail.austral.edu.ar>
2025-11-22 16:03:42 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,9 @@
-def check_vowels():
-    nombre = input('Ingrese un nombre: ').strip().lower()
-    print('Contiene a:','a' in nombre)
-    print('Contiene e:','e' in nombre)
-    print('Contiene i:','i' in nombre)
-    print('Contiene o:','o' in nombre)
-    print('Contiene u:','u' in nombre)
+nombre = input("Ingrese un nombre: ")
+
+nombre = nombre.lower()   # para ignorar mayúsculas
+
+print("Contiene a:", "a" in nombre)
+print("Contiene e:", "e" in nombre)
+print("Contiene i:", "i" in nombre)
+print("Contiene o:", "o" in nombre)
+print("Contiene u:", "u" in nombre)
