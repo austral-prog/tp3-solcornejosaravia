fc5741ffd4f2ca8254faf5526ff55976479ab3ba
solcornejosaravia <scornejosaravia@mail.austral.edu.ar>
2025-11-22 16:07:19 -0300

    Refactor input handling and print statements

--- in_string.py
+++ in_string.py
@@ -1,9 +1,7 @@
-nombre = input("Ingrese un nombre: ")
+nombre = input().lower()
 
-nombre = nombre.lower()
-
-print("Contiene a:", "a" in nombre)
-print("Contiene e:", "e" in nombre)
-print("Contiene i:", "i" in nombre)
-print("Contiene o:", "o" in nombre)
-print("Contiene u:", "u" in nombre)
+print(f"Contiene a: {'a' in nombre}")
+print(f"Contiene e: {'e' in nombre}")
+print(f"Contiene i: {'i' in nombre}")
+print(f"Contiene o: {'o' in nombre}")
+print(f"Contiene u: {'u' in nombre}")
