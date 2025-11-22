a7e8705f80589a826210ff8097a5544b38a718b8
solcornejosaravia <scornejosaravia@mail.austral.edu.ar>
2025-11-22 16:08:40 -0300

    Refactor vowel checks into a separate function

--- in_string.py
+++ in_string.py
@@ -1,7 +1,9 @@
 nombre = input().lower()
 
-print(f"Contiene a: {'a' in nombre}")
-print(f"Contiene e: {'e' in nombre}")
-print(f"Contiene i: {'i' in nombre}")
-print(f"Contiene o: {'o' in nombre}")
-print(f"Contiene u: {'u' in nombre}")
+def check_vowels():
+  print(f"Contiene a: {'a' in nombre}")
+  print(f"Contiene e: {'e' in nombre}")
+  print(f"Contiene i: {'i' in nombre}")
+  print(f"Contiene o: {'o' in nombre}")
+  print(f"Contiene u: {'u' in nombre}")
+check_vowels(nombre)
